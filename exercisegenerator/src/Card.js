98ebb41104bfff4f7effd56588128c7d3d8967c5
Keith Bailey <bailey.keith.allen@gmail.com>
2020-11-20 13:19:54 -0500

    fixed manifest issue

---
+++ exercisegenerator/src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css'
+
+// takes a pic and name, returns a new component to display the data
+const Card = ({ pic, name }) => {
+    return (
+        <div className='tc grow bg-light-green br3 pa3 ma1 dib bw1 shadow-5'>
+            <img alt={name} src={pic}/>            
+            <h3>{name}</h3>            
+        </div>
+    );
+}
+
+export default Card;
---
+++ exercisegenerator/src/lifts.js
@@ -0,0 +1,101 @@
+export const lifts = [
+    {
+        type: 'push',
+        name: 'Dip',
+        src: './pics/dips.png'
+    },
+    {
+        type: 'push',
+        name: 'Push-up',
+        src: './pics/pushup.jpg'
+    },
+    {
+        type: 'push',
+        name: 'Wide Grip Push-up',
+        src: './pics/widegrippushups.jpg'
+    },
+    {
+        type: 'push',
+        name: 'Cobra Push-up',
+        src: './pics/cobra.jpg'
+    },
+    {
+        type: 'push',
+        name: 'Overhead Press',
+        src: './pics/overheadpress.jpg'
+    },
+    {
+        type: 'pull',
+        name: 'Pullup',
+        src: './pics/pullup.jpg' 
+    },
+    {
+        type: 'pull',
+        name: 'Inverted Row',
+        src: './pics/invertedrow.png'
+    },
+    {
+        type: 'pull',
+        name: 'Bent Over Row',
+        src: './pics/bentrow.jpg'
+    }, {
+        type: 'pull',
+        name: 'Chin-up',
+        src: './pics/chinups.png'
+    },
+    {
+        type: 'pull',
+        name: 'Curl',
+        src: './pics/curl.jpg'
+    },
+    {
+        type: 'anterior',
+        name: 'Squat',
+        src: './pics/squat.jpg'
+    },
+    {
+        type: 'anterior',
+        name: 'Bulgarian Squat',
+        src: './pics/bulgarian.jpg'
+    },
+    {
+        type: 'anterior',
+        name: 'Side Lunge',
+        src: './pics/sidelunge.png'
+    },
+    {
+        type: 'anterior',
+        name: 'Step-up',
+        src: './pics/stepup.jpg'
+    },
+    {
+        type: 'anterior',
+        name: 'Pistol',
+        src: './pics/pistol.jpg'
+    },
+    {
+        type: 'posterior',
+        name: 'Deadlift',
+        src: './pics/deadlift.png'
+    },
+    {
+        type: 'posterior',
+        name: 'Hip Bridge',
+        src: './pics/hiphinge.jpg'
+    },
+    {
+        type: 'posterior',
+        name: 'RDL',
+        src: './pics/rdl.jpg'
+    },
+    {
+        type: 'posterior',
+        name: 'Hyper Extension',
+        src: './pics/hyper.jpg'
+    },
+    {
+        type: 'posterior',
+        name: 'Superman',
+        src: './pics/superman.jpg'
+    }
+]
