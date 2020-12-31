fd86698c09fc612807d6a3421f091dcabe32f3f1
Keith Bailey <bailey.keith.allen@gmail.com>
2020-12-31 05:23:16 -0500

    card padding to 2 from 3

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import './Card.css'
 // takes a pic and name, returns a new component to display the data
 const Card = ({ pic, name }) => {
     return (
-        <div className='tc grow bg-light-green br3 pa3 ma1 dib bw1 shadow-5'>
+        <div className='tc grow bg-light-green br3 pa2 ma1 dib bw1 shadow-5'>
             <img alt={name} src={pic}/>            
             <h3>{name}</h3>            
         </div>
